48e91d028f0791564d19b97f59e66fa4cf9dc619
Sirichandana-23 <nayakisirichandana@gmail.com>
2025-06-30 14:38:43 +0530

    th commit

---
+++ Basic.py
@@ -0,0 +1,13 @@
+#a=10
+#b,c=20,30
+#d=e=f=40
+#print(a)
+#print(b)
+#print(e)
+
+a=int(int input("Enter a value"))
+b=(int input("Enter b value"))
+print(a)
+print(a+b)
+d,e,f=map(int,input("Enter d,e,f"),split()) #10 20
+print(e)
---
+++ Variables.py
@@ -0,0 +1,5 @@
+a=int(input("Enter pretty number"))
+if(a%5==7 || a/5==7):
+    print("lucky no")
+else:
+    print("not lucky")
